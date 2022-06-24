4f93a202def38b61de5473730d498460cf6f077f
sonpt <ptson117@gmail.com>
2022-06-24 23:46:48 +0700

    base client benchmark

--- src/main/java/com/battle/app/BenchmarkClient.java
+++ src/main/java/com/battle/app/BenchmarkClient.java
@@ -10,6 +10,8 @@ import com.battle.utils.YamlUtils;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 import io.netty.util.internal.logging.Log4J2LoggerFactory;
 import io.vertx.core.json.JsonObject;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -18,28 +20,27 @@ import org.jctools.queues.MpmcArrayQueue;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.LockSupport;
 
 public class BenchmarkClient {
     private static final Logger logger = LogManager.getLogger(BenchmarkClient.class);
     private static final Integer concurrency = Integer.getInteger("concurrency", 10);
-    private static final Integer numUser = Integer.getInteger("numUser", 10000);
-    private static final Integer[] update = new Integer[]{2, 6, 2};
+    private static final Integer numUser = Integer.getInteger("numUser", 1000);
     private static final String host = System.getProperty("host", "localhost");
     private static final Integer port = Integer.getInteger("port", 9000);
-    private static final IJsonService json = new GsonService();
-    private static final MpmcArrayQueue<SampleRequest> queueRequest = new MpmcArrayQueue<>(10000);
+    private static final MpmcArrayQueue<Pair<User, SampleRequest>> queueRequest = new MpmcArrayQueue<>(10000);
     private static final MpmcArrayQueue<SampleResponse> queueResponse = new MpmcArrayQueue<>(10000);
-    private static final Map<String, SampleResponse> mapCounter = new ConcurrentHashMap<>();
     private static final Map<Long, SentInfo> mapWait = new ConcurrentHashMap<>();
     private static final MetricsBenchmark mbDeser = new MetricsBenchmark();
     private static final MetricsBenchmark mbSer = new MetricsBenchmark();
     private static final Map<Integer, MetricsBenchmark> mapMb = new HashMap<>();
+    private static final Integer maxUpdate = Integer.getInteger("maxUpdate", 10);
+    private static final MpmcArrayQueue<User> qUser = new MpmcArrayQueue<>(numUser);
 
     public static void main(String[] args) throws Exception {
         InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE);
@@ -50,49 +51,47 @@ public class BenchmarkClient {
             File log4j = new File("./etc/log4j2.xml");
             ctx.setConfigLocation(log4j.toURI());
         }
+        String payload = "{\"event\":\"walk\",\"data\":{\"gps\":[110000,110000],\"grav\":[12,212,122],\"step\":111}}";
+        JsonObject data = new JsonObject(payload);
         ClientConfiguration configuration = new ClientConfiguration();
         YamlUtils.objectToYamlFile(configuration, "etc/clientConf.yml");
         Client client = new Client(configuration, new IClientHandler() {
             @Override
             public void onResponse(InternalMessage message, ISenderInfo sender) {
-                long receive = System.currentTimeMillis();
+                long receive = System.nanoTime();
                 JsonObject obj = new JsonObject(message.getData());
                 int length = message.getData().getBytes().length;
                 mbDeser.statisticMetris(receive, length, "Deserialize");
                 Long requestId = obj.getLong("requestId");
-                SampleResponse response = new SampleResponse(requestId, obj.getString("userID"));
-                SentInfo sentInfo = mapWait.remove(requestId);
+                SentInfo sentInfo = mapWait.get(requestId);
                 if (sentInfo != null) {
                     sentInfo.getWait().complete(true);
-                    Integer requestType = sentInfo.getRequestType();
+                    Integer requestType = getRequestTypeFromUser(sentInfo.getUser());
+                    SampleResponse response = new SampleResponse(requestId, obj.getString("userID"), requestType);
                     if (!mapMb.containsKey(requestType)) {
                         mapMb.put(requestType, new MetricsBenchmark());
                     }
                     mapMb.get(requestType).statisticMetris(sentInfo.getSentTime(), length, getRequestTypeString(requestType));
+                    doEnqueue(response, queueResponse);
                 } else {
                     logger.warn("No sent info for request {} ", requestId);
                 }
-                boolean offer = false;
-                do {
-                    offer = queueResponse.offer(response);
-                    if (!offer) {
-                        LockSupport.parkNanos(10000);
-                    }
-                } while (!offer);
             }
         });
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
+
+
+        for (int i = 0; i < numUser; i++) {
+            qUser.offer(new User());
+        }
+        Thread threadInit = new Thread(new Runnable() {
             @Override
             public void run() {
-//                Map<String, >
-                for (int i = 0; i < numUser; i++) {
-
-                }
                 while (true) {
                     try {
-                        SampleResponse poll = queueResponse.poll();
-                        if (poll != null) {
-                            poll.getRequestId();
+                        User user = qUser.poll();
+                        if (user != null) {
+                            SampleRequest sampleRequest = new SampleRequest(user.getUserId(), 1);
+                            doEnqueue(new ImmutablePair<>(user, sampleRequest), queueRequest);
                         } else {
                             LockSupport.parkNanos(10000);
                         }
@@ -102,30 +101,73 @@ public class BenchmarkClient {
                 }
             }
         });
+        threadInit.setName("init-request");
+        threadInit.start();
+
+        Thread tResponse = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        SampleResponse poll = queueResponse.poll();
+                        if (poll != null) {
+                            Long requestId = poll.getRequestId();
+                            String userId = poll.getUserId();
+                            Integer requestType = poll.getRequestType();
+                            SentInfo sentInfo = mapWait.remove(requestId);
+                            //init thi gui update
+                            if (sentInfo == null) {
+                                logger.error("no sent info for request {}", requestId);
+                                continue;
+                            }
+                            User user = sentInfo.getUser();
+                            if (requestType == 1) {
+                                SampleRequest rq = new SampleRequest(userId, 2);
+                                rq.getTransactions().put(System.currentTimeMillis(), data);
+                                doEnqueue(new ImmutablePair<>(user, rq), queueRequest);
+                            } else if (requestType == 2) {
+                                int i = sentInfo.getUser().getCountUpdate().get();
+                                int type = i < maxUpdate ? 2 : 3;
+                                SampleRequest rq = new SampleRequest(userId, type);
+                                rq.getTransactions().put(System.currentTimeMillis(), data);
+                                doEnqueue(new ImmutablePair<>(user, rq), queueRequest);
+                            } else {
+                                doEnqueue(new User(), qUser);
+                            }
+                        } else {
+                            LockSupport.parkNanos(10000);
+                        }
+                    } catch (Exception ex) {
+                        logger.error(ex, ex);
+                    }
+                }
+            }
+        });
+        tResponse.setName("response-handler");
+        tResponse.start();
 
         for (int i = 0; i < concurrency; i++) {
-            Executors.newSingleThreadExecutor().execute(new Runnable() {
+            Thread tSend = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     logger.info("Start send");
                     while (true) {
-                        SampleRequest poll = queueRequest.poll();
+                        Pair<User, SampleRequest> poll = queueRequest.poll();
                         if (poll != null) {
-                            String s = JsonObject.mapFrom(poll).toString();
-                            SentInfo sentInfo = new SentInfo(poll.getType(), poll.getRequestID());
-                            mapWait.put(poll.getRequestID(), sentInfo);
+                            long l = System.nanoTime();
+                            SampleRequest request = poll.getValue();
+                            User user = poll.getKey();
+                            String s = JsonObject.mapFrom(request).toString();
+                            mbSer.statisticMetris(l, s.getBytes().length, "Serialize");
+                            SentInfo sentInfo = new SentInfo(user, request.getRequestID());
+                            mapWait.put(request.getRequestID(), sentInfo);
                             sentInfo.setSentTime(System.nanoTime());
                             try {
                                 client.send(host, port, new InternalMessage(s));
+                                sentInfo.getWait().get();
                             } catch (Exception e) {
                                 logger.error(e, e);
-                            }
-                            try {
-                                sentInfo.getWait().get();
-                            } catch (InterruptedException e) {
-                                logger.error(e, e);
-                            } catch (ExecutionException e) {
-                                logger.error(e, e);
+                                doEnqueue(poll, queueRequest);
                             }
                         } else {
                             LockSupport.parkNanos(100000);
@@ -133,7 +175,30 @@ public class BenchmarkClient {
                     }
                 }
             });
+            tSend.setName("send-" + i);
+            tSend.start();
         }
+    }
+
+    private static Integer getRequestTypeFromUser(User user) {
+        AtomicInteger countUpdate = user.getCountUpdate();
+        if (countUpdate.get() == 0) {
+            return 1;
+        }
+        if (countUpdate.get() < maxUpdate) {
+            return 2;
+        }
+        return 3;
+    }
+
+    private static <T> void doEnqueue(T obj, MpmcArrayQueue<T> queue) {
+        boolean offer = false;
+        do {
+            offer = queue.offer(obj);
+            if (!offer) {
+                LockSupport.parkNanos(10000);
+            }
+        } while (!offer);
     }
 
     private static String getRequestTypeString(Integer requestType) {
@@ -149,8 +214,24 @@ public class BenchmarkClient {
         return "Unknown";
     }
 
+    private static class User {
+        private final String userId = UUID.randomUUID().toString();
+        private final AtomicInteger countUpdate = new AtomicInteger();
+
+        public User() {
+        }
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public AtomicInteger getCountUpdate() {
+            return countUpdate;
+        }
+    }
+
     private static class SentInfo {
-        private final Integer requestType;
+        private final User user;
         private final Long requestID;
         private final CompletableFuture<Boolean> wait = new CompletableFuture();
 
@@ -164,13 +245,13 @@ public class BenchmarkClient {
             this.sentTime = sentTime;
         }
 
-        public SentInfo(Integer requestType, Long requestID) {
-            this.requestType = requestType;
+        public SentInfo(User user, Long requestID) {
+            this.user = user;
             this.requestID = requestID;
         }
 
-        public Integer getRequestType() {
-            return requestType;
+        public User getUser() {
+            return user;
         }
 
         public Long getRequestID() {
--- src/main/java/com/battle/app/SampleResponse.java
+++ src/main/java/com/battle/app/SampleResponse.java
@@ -3,10 +3,16 @@ package com.battle.app;
 public class SampleResponse {
     public final Long requestID;
     public final String userID;
+    public final Integer requestType;
 
-    public SampleResponse(Long requestId, String userId) {
+    public SampleResponse(Long requestId, String userId, Integer requestType) {
         this.requestID = requestId;
         this.userID = userId;
+        this.requestType = requestType;
+    }
+
+    public Integer getRequestType() {
+        return requestType;
     }
 
     public Long getRequestId() {
